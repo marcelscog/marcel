9361f8f3a684b3e4e89b7c5b1039f931f45eb2e0
Marcel Lourenço <marcelscognamiglio@MacBook-Pro-de-Marcel.local>
2017-02-06 16:15:21 -0200

    Desce

---
+++ contact.php
@@ -0,0 +1,28 @@
+<?php
+$from = "marcel.scognamiglio@gmail.com";
+$to = "marcel.baka@gmail.com";
+$name = Trim(stripslashes($_POST['name']));
+$email = Trim(stripslashes($_POST['email']));
+$message = Trim(stripslashes($_POST['message']));
+$promo = Trim(stripslashes($_POST['promo']));
+$body ="";
+$body .="Name: ";
+$body .=$name;
+$body .="\n";
+$body .="Email: ";
+$body .=$email;
+$body .="\n";
+$body .="Message: ";
+$body .=$message;
+$body .="\n";
+$body .="Promo accepted? ";
+$body .=$promo;
+$body .="\n";
+$go = mail($to, $subject, $body, "From:<$from>");
+if($go){
+	print("Success!");
+}
+else{
+	print("Unable to send!!");
+}
+?>
